289e20b38244a8efa046879cebb01356cabfb774
Iyad Ahmed <iyadahmed430@gmail.com>
2023-08-14 21:11:06 +0300

    Index URLs using page keywords

--- crawler.py
+++ crawler.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+import pickle
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service as ChromeService
@@ -16,6 +19,8 @@ driver = webdriver.Chrome(
 urls = ["https://www.wikipedia.org/"]
 visited = set()
 
+engine_index: dict[set[str]] = defaultdict(set)
+
 for _ in range(MAX_PAGES):
     if len(urls) == 0:
         break
@@ -26,8 +31,9 @@ for _ in range(MAX_PAGES):
 
     soup = BeautifulSoup(driver.page_source, features="lxml")
     clean_text = soup.get_text()
-    print(clean_text)
-    # TODO: split clean text into words and index web page to search it later
+
+    for word in clean_text.split():
+        engine_index[word.lower()].add(url)
 
     for e in driver.find_elements(By.CSS_SELECTOR, "a[href]"):
         sub_url = e.get_attribute("href")
@@ -37,3 +43,8 @@ for _ in range(MAX_PAGES):
         urls.append(sub_url)
 
 driver.quit()
+
+
+# Dump engine index
+with open("engine_index.bin", "wb") as file:
+    pickle.dump(engine_index, file)
