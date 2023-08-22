3733306607782d90373a1de7e845a256c65837f0
Iyad Ahmed <iyadahmed430@gmail.com>
2023-08-23 01:09:02 +0300

    Remove obsolete files

--- crawler_state.py
+++
@@ -1,8 +0,0 @@
-from collections import defaultdict
-
-
-class CrawlerState:
-    def __init__(self):
-        self.engine_index: defaultdict[str, set[str]] = defaultdict(set)
-        self.visited_urls: set[str] = set()
-        self.urls_queue: list[str] = []
