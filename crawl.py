fbaaeee387c63c9359f39c7892b7530270de8fb1
Iyad Ahmed <iyadahmed430@gmail.com>
2023-08-23 04:11:15 +0300

    Output crawl result to a CSV file

--- crawl.py
+++ crawl.py
@@ -1,3 +1,4 @@
+import csv
 from collections import deque
 from pathlib import Path
 
@@ -13,29 +14,29 @@ gecko_service = FirefoxService(gecko_path, log_output="geckodriver.log")
 gecko_driver = webdriver.Firefox(service=gecko_service, options=gecko_options)
 
 urls_queue: deque[str] = deque(["https://www.wikipedia.org/"])
+with open("crawl_result.csv", "w") as csv_file:
+    csv_writer = csv.writer(csv_file)
+    csv_writer.writerow(["text", "url"])
 
+    def crawl(url: str):
+        gecko_driver.get(url)
 
-def crawl(url: str):
-    gecko_driver.get(url)
+        clean_text = BeautifulSoup(gecko_driver.page_source, "lxml").get_text(
+            separator=" ", strip=True
+        )
+        csv_writer.writerow([clean_text, url])
 
-    clean_text = BeautifulSoup(gecko_driver.page_source, "lxml").get_text(
-        separator=" ", strip=True
-    )
-    print(clean_text)
-    # TODO: Index page content
+        # Find all links
+        link_elements = gecko_driver.find_elements(By.TAG_NAME, "a")
+        for e in link_elements:
+            try:
+                link = e.get_attribute("href")
+                if link is not None:
+                    urls_queue.append(link)
+            except Exception:
+                continue
 
-    # Find all links
-    link_elements = gecko_driver.find_elements(By.TAG_NAME, "a")
-    for e in link_elements:
-        try:
-            link = e.get_attribute("href")
-            if link is not None:
-                urls_queue.append(link)
-        except Exception:
-            continue
+    while len(urls_queue) > 0:
+        crawl(urls_queue.popleft())
 
-
-while len(urls_queue) > 0:
-    crawl(urls_queue.popleft())
-
-gecko_driver.quit()
+    gecko_driver.quit()
